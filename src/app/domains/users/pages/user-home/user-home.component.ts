a25e368793490335435c8a3857121008d5e8208c
lgonzaloquijada <lgonzalo.quijada@gmail.com>
2025-01-22 01:06:37 -0700

    feat: users form to ang material and validations.

--- src/app/domains/users/pages/user-home/user-home.component.ts
+++ src/app/domains/users/pages/user-home/user-home.component.ts
@@ -1,24 +1,15 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { UserTableComponent } from '@domains/users/components/user-table/user-table.component';
-import { UserService } from '@services/user.service';
 
 @Component({
   selector: 'app-user-home',
-  imports: [UserTableComponent, MatIconModule],
+  imports: [UserTableComponent, MatIconModule, MatButtonModule, RouterLink],
   templateUrl: './user-home.component.html',
   styleUrl: './user-home.component.scss',
 })
-export class UserHomeComponent implements OnInit {
-  private userService = inject(UserService);
-  private router = inject(Router);
-
+export class UserHomeComponent {
   constructor() {}
-
-  ngOnInit() {}
-
-  addUser() {
-    this.router.navigate(['/users/add']);
-  }
 }
